d17d732436b5722a0bfa815d9313394fa0fe3220
OyedunOye <oyesinaoyedun@yahoo.com>
2023-11-19 22:25:08 +0100

    unit test added

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,22 @@
 import { render, screen } from '@testing-library/react';
-import App from './App';
+import BookingForm from './components/reserveTableForm/BookingForm.js';
+import Main from './components/main/Main.js';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+
+test('Renders the BookingForm label, Choose date', () => {
+  render(<BookingForm />);
+  const labelElement = screen.getByText("Choose date");
+  expect(labelElement).toBeInTheDocument();
 });
+
+test('initializeTimes fetches available times', () => {
+  // Mock the fetchAPI function to return a sample array of available times
+  const mockFetchAPI = jest.fn(() => ['17:00', '17:30', '18:00']);
+
+  // Render the Main component with the mocked fetchAPI function
+  render(<Main />);
+
+  // Assert that the fetchAPI function was called with the current date
+  expect(mockFetchAPI).toHaveBeenCalledWith(new Date());
+});
+
