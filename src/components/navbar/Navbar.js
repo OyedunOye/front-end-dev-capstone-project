4efda3623e8713d91c1c7ae122ae5df30bf1df68
OyedunOye <oyesinaoyedun@yahoo.com>
2024-01-15 23:27:21 +0100

    modification to menu button display

--- src/components/navbar/Navbar.js
+++ src/components/navbar/Navbar.js
@@ -7,6 +7,8 @@ import { Link } from 'react-router-dom';
 // I have used icons from react icons, I first needed to 
 //install react icons with npm before I can install and use in my code. Just a reminder for myself.
 //Remember to fix the drop-down display for menu icon in mobile devices.
+//Drop-down for mobile app improved, maybe a little more 
+//improvement could help more (15-Jan-24)
 const Navbar = () => {
   const [toggleMenu, setToggleMenu] = useState(false);
 
@@ -27,11 +29,13 @@ const Navbar = () => {
           </nav>
         </div>
         <div className='litmon_navbar-menu'>
-      {toggleMenu
-          ?<RiCloseLine color='000' size={27} onClick={()=> setToggleMenu(false)}/>
-          :<RiMenuFill color='000' size={27} onClick={()=> setToggleMenu(true)}/>
-        }
-        {toggleMenu && (
+          <div className='menu_button'>
+            {toggleMenu
+              ?<RiCloseLine color='000' size={27} onClick={()=> setToggleMenu(false)}/>
+              :<RiMenuFill color='000' size={27} onClick={()=> setToggleMenu(true)}/>
+            }
+          </div>
+          {toggleMenu && (
           <div className='litmon_navbar-menu_container scale-up-center'>
             <div className='litmon_navbar-menu_container-links' >
               <nav>
